f3016aa788fa0fb9d9fa35e9debaa32db44343cc
mehul-1009 <mehul.parmar@entanglesoftware.com>
2025-05-06 18:56:28 +0530

    add api for cart

---
+++ tests/crm/commerce/carts/basic_test.go
@@ -0,0 +1,176 @@
+package carts_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/entanglesoftware/hubspot-api-go/codegen/crm/commerce/carts"
+	"github.com/entanglesoftware/hubspot-api-go/configuration"
+	"github.com/entanglesoftware/hubspot-api-go/discovery/crm"
+)
+
+func TestSaveCarts(t *testing.T) {
+	config := configuration.Configuration{
+		BasePath:               configuration.BaseURL,
+		NumberOfAPICallRetries: 3,
+	}
+	crm := crm.NewCrmDiscovery(&config)
+
+	// Initialize a variable of type Carts
+	cart := carts.CreateCartJSONRequestBody{
+		Properties: map[string]string{
+			"hs_cart_name":     "Cart Name",
+			"hs_cart_discount": "10",
+		},
+	}
+
+	ct := crm.Carts()
+	response, err := ct.CreateCartWithResponse(context.Background(), cart)
+	if err != nil {
+		t.Fatalf("API call failed: %v", err)
+	}
+
+	if response.StatusCode() == 201 {
+		if response.JSON201 == nil || response.JSON201.Id == "" {
+			t.Fatalf("Response contains no results")
+		}
+
+		if response.JSON201.Properties != nil {
+			t.Logf("Properties: %s", response.JSON201.Properties)
+		} else {
+			t.Log("No properties found.")
+		}
+	} else {
+		t.Fatalf("Test Failed with status code %d: %v", response.StatusCode(), response)
+	}
+}
+
+func TestRetrieveCart(t *testing.T) {
+	config := configuration.Configuration{
+		BasePath:               configuration.BaseURL,
+		NumberOfAPICallRetries: 3,
+	}
+	crm := crm.NewCrmDiscovery(&config)
+
+	cartId := "427642968875"
+
+	// Initialize a variable of type Carts
+	cart := carts.GetCartByIdParams{
+		Properties: &[]string{"hs_cart_name", "hs_cart_discount"},
+	}
+
+	ct := crm.Carts()
+	response, err := ct.GetCartByIdWithResponse(context.Background(), cartId, &cart)
+	if err != nil {
+		t.Fatalf("API call failed: %v", err)
+	}
+
+	if response.StatusCode() == 200 {
+		if response.JSON200 == nil || response.JSON200.Id == "" {
+			t.Fatalf("Response contains no results")
+		}
+
+		if response.JSON200.Properties != nil {
+			t.Logf("Properties: %s", response.JSON200.Properties)
+		} else {
+			t.Log("No properties found.")
+		}
+	} else {
+		t.Fatalf("Test Failed with status code %d: %v", response.StatusCode(), response)
+	}
+}
+
+func TestRetrieveCarts(t *testing.T) {
+	config := configuration.Configuration{
+		BasePath:               configuration.BaseURL,
+		NumberOfAPICallRetries: 3,
+	}
+	crm := crm.NewCrmDiscovery(&config)
+
+	// Initialize a variable of type Carts
+	cart := carts.GetCartsParams{}
+
+	ct := crm.Carts()
+	response, err := ct.GetCartsWithResponse(context.Background(), &cart)
+	if err != nil {
+		t.Fatalf("API call failed: %v", err)
+	}
+
+	if response.StatusCode() == 200 {
+		if response.JSON200 == nil || response.JSON200.Results == nil {
+			t.Fatalf("Response contains no results")
+		}
+
+		for _, result := range response.JSON200.Results {
+			t.Logf("%+v\n", result)
+			t.Log("-----")
+			if result.Properties != nil {
+				for key, value := range result.Properties {
+					t.Logf("Key: %s, Value: %+v\n", key, value)
+				}
+			} else {
+				t.Log("No properties found.")
+			}
+			t.Log("-----")
+		}
+	} else {
+		t.Fatalf("Test Failed with status code %d: %v", response.StatusCode(), response)
+	}
+}
+
+func TestUpdateCart(t *testing.T) {
+	config := configuration.Configuration{
+		BasePath:               configuration.BaseURL,
+		NumberOfAPICallRetries: 3,
+	}
+	crm := crm.NewCrmDiscovery(&config)
+
+	cartId := "427637542790"
+
+	// Initialize a variable of type Carts
+	cart := carts.UpdateCartJSONRequestBody{
+		Properties: map[string]string{
+			"hs_cart_name": "Updated Cart Name",
+		},
+	}
+
+	ct := crm.Carts()
+	response, err := ct.UpdateCartWithResponse(context.Background(), cartId, cart)
+	if err != nil {
+		t.Fatalf("API call failed: %v", err)
+	}
+
+	if response.StatusCode() == 200 {
+		if response.JSON200 == nil || response.JSON200.Properties == nil {
+			t.Fatalf("Response contains no results")
+		}
+
+		for key, value := range response.JSON200.Properties {
+			t.Logf("Key: %s, Value: %+v\n", key, value)
+		}
+	} else {
+		t.Fatalf("Test Failed with status code %d: %v", response.StatusCode(), response)
+	}
+}
+
+func TestDeleteCart(t *testing.T) {
+	config := configuration.Configuration{
+		BasePath:               configuration.BaseURL,
+		NumberOfAPICallRetries: 3,
+	}
+	crm := crm.NewCrmDiscovery(&config)
+
+	cartId := "427637542790"
+
+	ct := crm.Carts()
+	response, err := ct.DeleteCartByIdWithResponse(context.Background(), cartId)
+	if err != nil {
+		t.Fatalf("API call failed: %v", err)
+	}
+
+	if response.StatusCode() == 204 {
+		t.Logf("Cart Deleted")
+	} else {
+		t.Fatalf("Test Failed with status code %d: %v", response.StatusCode(), response)
+	}
+}
