a4a5b4a9de621362100d8e57fced5af3fa66f4eb
Sumitk99 <sumit2k19kumar@gmail.com>
2025-05-12 09:31:52 +0530

    Minor changes

--- tests/testsutil/initiateClient.go
+++ tests/testsutil/initiateClient.go
@@ -3,9 +3,7 @@ package testsutil
 import (
 	"github.com/entanglesoftware/hubspot-api-go/configuration"
 	"github.com/entanglesoftware/hubspot-api-go/discovery/crm"
-	"github.com/entanglesoftware/hubspot-api-go/util/decorator"
 	"log"
-	"os"
 	"sync"
 )
 
@@ -20,7 +18,6 @@ func GetClient() *crm.CrmDiscovery {
 		config := configuration.Configuration{
 			BasePath:               configuration.BaseURL,
 			NumberOfAPICallRetries: 3,
-			AuthDecorator:          decorator.NewAuthDecorator(os.Getenv("HS_ACCESS_TOKEN")),
 		}
 		client = crm.NewCrmDiscovery(&config)
 	})
